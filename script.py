6e8d8821246a730b5df6064d0be2c41b6155f9d6
piyu5hkumar <piyushkumar031997@gmail.com>
2022-08-25 23:44:09 +0530

    updated script

--- script.py
+++ script.py
@@ -5,6 +5,8 @@ import json
 from pathlib import Path
 import os
 import dotenv
+from selenium.webdriver.common.keys import Keys
+
 
 dotenv_file = os.path.join(Path(__file__).resolve().parent, '.env')
 if os.path.isfile(dotenv_file):
@@ -19,8 +21,8 @@ class InstagramInfo():
     def __init__(self, headless=False):
         self.total_followers = 0
         self.total_followings = 0
-        self.all_followers = {}
-        self.all_followings = {}
+        self.all_followers = set()
+        self.all_followings = set()
 
         self.chrome_options = webdriver.ChromeOptions()
         self.chrome_options.headless = headless
@@ -36,7 +38,7 @@ class InstagramInfo():
             self.load_cookies()
             print('cookies loaded\n')
             return
-
+        
         try:
             self.driver.get("https://www.instagram.com/")
             sleep(3)
@@ -80,67 +82,97 @@ class InstagramInfo():
             return False
 
     def followings(self):
-        self.all_followings = {}
 
         self.driver.get('https://www.instagram.com/accounts/access_tool/accounts_you_follow')
 
+        tries = 0
         while True:
             try:
+                # breakpoint()
                 self.driver.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/article/main/button').click()
                 sleep(0.5)
-            except:
-                break
+            except Exception as e:
+                tries += 1
+                sleep(2)
+                if tries == 15:
+                    break
+                
 
         divs = self.driver.find_elements_by_class_name('-utLf')
 
         for div in divs:
             name = div.get_attribute('innerHTML')
-            if name not in self.all_followings:
-                self.all_followings[name] = True
+            self.all_followings.add(name)
 
         self.total_followings = len(self.all_followings)
 
+
     def followers(self):
-        self.all_followers = {}
 
         self.driver.get('https://www.instagram.com/accounts/access_tool/accounts_following_you')
-
+        tries = 0
         while True:
             try:
                 self.driver.find_element_by_xpath(
                     '//*[@id="react-root"]/section/main/div/article/main/button').click()
                 sleep(0.5)
             except:
-                break
+                tries += 1
+                sleep(2)
+                if tries == 15:
+                    break
 
         divs = self.driver.find_elements_by_class_name('-utLf')
 
         for div in divs:
             name = div.get_attribute('innerHTML')
-            if name not in self.all_followers:
-                self.all_followers[name] = True
+            self.all_followers.add(name)
 
         self.total_followers = len(self.all_followers)
 
     def analyze(self):
-        self.followers()
-        self.followings()
-        print('total followings:', self.total_followings)
-        print('total followers:', self.total_followers)
+        self.driver.get('https://www.instagram.com/accounts/edit/')
+        current_bio_element = self.driver.find_element_by_xpath(
+                    '//*[@id="pepBio"]')
+        sleep(2)
+        'current hour is ='
+        'I am updating it every hour :)'
+        current_bio = current_bio_element.get_attribute('innerHTML')
 
-    def not_following_back(self):
-        for following in self.all_followings:
-            if following not in self.all_followers:
-                print(f'{following} does\'nt follow you back')
+        before_time_part, after_time_part = current_bio.split('~')
 
-    def not_follow_back(self):
-        for follower in self.all_followers:
-            if follower not in self.all_followings:
-                print(f'you are not following {follower} back')
+        import pytz
+        from datetime import datetime
+        tz = pytz.timezone('Asia/Kolkata')
+        india_now = datetime.now(tz)
+        india_now_hour_with_am_pm = india_now.strftime('%I %p')
 
+        new_bio = before_time_part + india_now_hour_with_am_pm + after_time_part
 
+        current_bio_element.send_keys(Keys.CONTROL, 'a')
+        current_bio_element.send_keys(Keys.BACK_SPACE)
+        current_bio_element.send_keys(new_bio)
+
+        sleep(1)
+        self.driver.find_element_by_xpath(
+                    '//*[@id="react-root"]/section/main/div/article/form/div[10]/div/div/button').click()
+        
+        # current_bio_element_clicked
 instagram_info = InstagramInfo()
 instagram_info.login(os.environ.get('USER_NAME'), os.environ.get('PASSWORD'))
-instagram_info.analyze()
-instagram_info.not_following_back()
+# instagram_info.analyze()
+instagram_info.followings()
+instagram_info.followers()
+# print(instagram_info.all_followers)
+# print(instagram_info.all_followings)
+not_following_back = instagram_info.all_followings - instagram_info.all_followers
+
+print('not following back - ')
+for follow in not_following_back:
+    print(follow)
+
+print('\nI am not following back - ')
+m_not_following_back =  instagram_info.all_followers - instagram_info.all_followings
+for follow in m_not_following_back:
+    print(follow)
